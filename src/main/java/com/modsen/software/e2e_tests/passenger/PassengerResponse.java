06487c21825f30b08f7e10715fffe176f8141d9d
aleksej <man747704@gmail.com>
2024-11-01 05:23:34 +0300

    feat(test): add e2e tests for Passenger service

---
+++ src/main/java/com/modsen/software/e2e_tests/passenger/PassengerResponse.java
@@ -0,0 +1,24 @@
+package com.modsen.software.e2e_tests.passenger;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class PassengerResponse {
+    private Long id;
+
+    private String fullName;
+
+    private String email;
+
+    private String phone;
+
+    private Boolean isDeleted;
+
+    private Float rating;
+}
